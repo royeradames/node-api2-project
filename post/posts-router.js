0a0eb142ef75d7682266daeebe5ce828adc2eafb
royeradames <royeraadames@gmail.com>
2020-09-01 18:45:22 -0400

    put and delete methods work

--- post/posts-router.js
+++ post/posts-router.js
@@ -20,6 +20,88 @@ router.get('/:id', async (req, res) => {
     res.status(200).json(post)
 })
 
+router.post('/', async (req, res) => {
+    // Creates a post using the information sent inside the request body
+    try {
+        // const posts = db.find()
+        let posts
+        await db.find()
+            .then(resp => {
+                posts = resp
+            })
+        const newPost = {
+            title: req.body.title,
+            contents: req.body.contents,
+        }
+
+        //title and contents most be pass down if not return an error
+        if (!newPost.title || !newPost.contents) {
+            res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' })
+        }
+        //add new post
+        posts.push(newPost)
+        // return post
+        res.status(201).json(posts)
+    } catch (error) {
+        res.status(500).json({ error: "There was an error while saving the post to the database" })
+    }
+})
+
+router.put('/:id', async (req, res) => {
+
+    try {
+        const id = Number(req.params.id)
+        let post
+        await db.findById(id)
+            .then(() => {
+
+                const updateComment = {
+                    title: req.body.title,
+                    contents: req.body.contents,
+                }
+                if (!updateComment.title || !updateComment.contents) res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+
+                // If the information about the comment is valid: 
+                // 201 return the newly created comment.
+                db.update(id, updateComment)
+                    .then(() => {
+                        //return the updated post
+                        db.findById(id)
+                            .then((updatedPost) => {
+                                res.status(201).json(updatedPost)
+                            })
+                    })
+                    .catch((err) => {
+                        res.status(400).json(err)
+
+                    })
+            })
+            .catch(() => {
+                res.status(400).json({ message: "The post with the specified ID does not exist." })
+            })
+    } catch (error) {
+        res.status(500).json({ error: "The post information could not be modified." })
+
+    }
+
+})
+router.delete('/:id', async (req, res) => {
+    try {
+        const id = Number(req.params.id)
+        db.remove(id)
+            .then(() => {
+                res.status(204).end()
+            })
+            .catch(() => {
+                res.status(400).json({ error: "The post could not be removed" })
+
+            })
+    } catch (error) {
+        res.status(500).json({ error: "The comments information could not be retrieved." })
+
+    }
+})
+
 // having trouble understanding how comments and posts are jointing
 router.get('/api/posts/:id/comments', async (req, res) => {
     const id = Number(req.params.id)
@@ -63,63 +145,5 @@ router.post('/:id/comments', async (req, res) => {
 
     }
 })
-
-router.post('/', async (req, res) => {
-    // Creates a post using the information sent inside the request body
-    try {
-        // const posts = db.find()
-        let posts
-        await db.find()
-            .then(resp => {
-                posts = resp
-            })
-        const newPost = {
-            title: req.body.title,
-            contents: req.body.contents,
-        }
-
-        //title and contents most be pass down if not return an error
-        if (!newPost.title || !newPost.contents) {
-            res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' })
-        }
-        //add new post
-        posts.push(newPost)
-        // return post
-        res.status(201).json(posts)
-    } catch (error) {
-        res.status(500).json({ error: "There was an error while saving the post to the database" })
-    }
-})
-
-router.put('/:id', async (req, res) => {
-
-    try {
-        const id = Number(req.params.id)
-        let post
-        await db.findById(id)
-            .then(resp => {
-                post = resp
-
-                const updateComment = {
-                    title: req.body.title,
-                    contents: req.body.contents,
-                }
-                if (!updateComment.title || !updateComment.contents) res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
-
-                // If the information about the comment is valid: 
-                // 201 return the newly created comment.
-                db.update(id,)
-                res.status(201).json(db.insertComment(newComment))
-            })
-            .catch(() => {
-                res.status(400).json({ message: "The post with the specified ID does not exist." })
-            })
-    } catch (error) {
-        res.status(500).json({ error: "The post information could not be modified." })
-
-    }
-
-})
-
 
 module.exports = router
